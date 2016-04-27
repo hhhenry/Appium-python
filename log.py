233b45b09b6a3f58043781dc6ae5c6555254496e
hhhenry <lihaoranhiei@163.com>
2016-04-27 17:50:37 +0800

    Update log.py

--- log.py
+++ log.py
@@ -7,7 +7,7 @@ class Log():
         self.logger.setLevel(logging.INFO)
 
         #create handler,write log
-        filename = "/Users/haoran.li/test/autoTest/sgLog.log"
+        filename = "/Users/henry/test/autoTest/sgLog.log"
         fh = logging.FileHandler(filename)
         #Define the output format of formatter handler
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
